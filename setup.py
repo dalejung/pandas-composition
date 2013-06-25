c10c7d106ffef8c747830b8559c2e50404b43fef
Dale Jung <dale@dalejung.com>
2013-06-25 19:22:32 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup                                                      
+                                                                                      
+DISTNAME='pandas_composition'                                                                 
+FULLVERSION='0.1'                                                                     
+                                                                                      
+setup(name=DISTNAME,                                                                  
+      version=FULLVERSION,                                                            
+      packages=['pandas_composition',
+               ]                                                         
+      )   
