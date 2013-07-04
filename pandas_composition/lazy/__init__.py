ebf474f3d7b0ddec83658dd67cefa56043000669
Dale Jung <dale@dalejung.com>
2013-07-03 20:27:45 -0400

    added first pass at LazyDataFrame

---
+++ pandas_composition/lazy/__init__.py
@@ -0,0 +1,123 @@
+from collections import deque
+
+import numpy as np
+import pandas as pd
+import numexpr as ne
+
+from pandas_composition import UserFrame
+
+deferred_op = ['__add__', '__sub__', '__mul__', '__div__', '__pow__']
+eval_op = ['__array__', '.values']
+op_trans = {}
+op_trans['__add__'] = '+'
+op_trans['__sub__'] = '-'
+op_trans['__mul__'] = '*'
+op_trans['__div__'] = '/'
+op_trans['__pow__'] = '**'
+
+class PandasExpression(object):
+    def __init__(self, right, op):
+        self.right = right
+        self.op = op
+
+def lazy(self):
+    return LazyFrame(self)
+
+pd.DataFrame.lazy = lazy
+
+class LazyFrame(UserFrame):
+    """
+    DataFrame that defers doing operations until it has to. 
+
+    An un-evaled LazyFrame will have an empty DataFrame for 
+    `self.pobj`.
+
+    Once a LazyFrame is evaled, it will act like an Ordinary
+    DataFrame. Or more precisely, a UserFrame.
+    """
+    def __init__(self, *args, **kwargs):
+        super(LazyFrame, self).__init__(*args, **kwargs)
+        self.expressions = deque() 
+        self.add_expr(self.pobj, None)
+        self.pobj = pd.DataFrame() # un evaluate
+
+    def add_expr(self, right, op):
+        expr = PandasExpression(right, op)
+        self.expressions.append(expr)
+
+    def eval(self):
+        # already evaled
+        if self.pobj:
+            return self.pobj
+
+        full, ns = self.gen_ne()
+        res = ne.evaluate(full, local_dict=ns)
+        # inplace
+        if res.ndim == 1:
+            pobj = pd.Series(res)
+            self.pobj = pobj
+        if res.ndim == 2:
+            pobj = pd.DataFrame(res)
+            self.pobj = pobj
+        return pobj
+
+    def gen_ne(self):
+        """
+        Generate the values needed for numexpr
+        Essentially a full string expression and a namespace
+        """
+        sub_count = 1
+        ns = {}
+        subs = []
+        for expr in self.expressions:
+            val = expr.right
+            op = expr.op
+            if np.isscalar(val):
+                val = repr(val)
+            else:
+                placeholder = '_pobj'+str(sub_count)
+                sub_count += 1
+                assert placeholder not in ns
+                ns[placeholder] = val
+                val = placeholder
+
+            if op is None:
+                subs.append(val)
+                continue
+            else:
+                op = op_trans[op]
+                subs.append("{op} {val}".format(op=op, val=val))
+        full = self._gen_full_string(subs)
+        return full, ns
+
+    def _gen_full_string(self, subs):
+        if len(subs) == 1:
+            full = subs[0]
+            return full
+
+        full = '({left} {right})'.format(left=subs[0], right=subs[1])
+        for s in subs[2:]:
+            full = '({left} {right})'.format(left=full, right=s)
+        return full
+
+    def _delegate(self, name, *args, **kwargs):
+        if name in deferred_op:
+            return self.defer_op(name, *args, **kwargs)
+        # if not deferred or part of pass safe-list
+        # we play it safe and eval
+        if name not in ['pobj']:
+            self.eval()
+        return super(LazyFrame, self)._delegate(name, *args, **kwargs)
+
+    def defer_op(self, name, *args, **kwargs):
+        stack = LazyFrame()
+        stack.expressions = deque(self.expressions)
+        stack.add_expr(args[0], name)
+        return stack
+
+    def __repr__(self):
+        if self.pobj:
+            return repr(self.pobj)
+        full, ns = self.gen_ne()
+        return "LazyFrame: \n{full}".format(full=full)
+
---
+++ pandas_composition/lazy/test/test_lazy.py
@@ -0,0 +1,66 @@
+from unittest import TestCase
+
+import numpy as np
+import pandas as pd
+from pandas_composition.lazy import LazyFrame
+import pandas.util.testing as tm
+
+df = pd.DataFrame(np.random.randn(10000, 5))
+lf = LazyFrame(df)
+
+class TestLazy(TestCase):
+
+    def __init__(self, *args, **kwargs):
+        TestCase.__init__(self, *args, **kwargs)
+
+    def runTest(self):
+        pass
+
+    def setUp(self):
+        pass
+
+    def test_simple_eval(self):
+        """ 
+        Test simple binary oeprations
+        """
+        correct = df + 1
+        test = lf + 1
+        assert not test.pobj
+        # following code should trigger eval
+        tm.assert_almost_equal(correct, test)
+
+    def test_simple_op(self):
+        """ 
+        Test simple binary oeprations
+        """
+        correct = df + 1
+        test = lf + 1
+        tm.assert_almost_equal(correct, test)
+
+        correct = df - 1
+        test = lf - 1
+        tm.assert_almost_equal(correct, test)
+
+        correct = df * 10.0
+        test = lf * 10.0
+        tm.assert_almost_equal(correct, test)
+
+        correct = df / 10.0
+        test = lf / 10.0
+        tm.assert_almost_equal(correct.values, test.values)
+
+        correct = df ** 10.0
+        test = lf ** 10.0
+        tm.assert_almost_equal(correct.values, test.values)
+
+    def test_complex(self):
+        """ 
+        Test simple binary oeprations
+        """
+        correct = df ** 10.0 + 1 + df * df
+        test = lf ** 10.0 + 1 + lf * lf
+        tm.assert_almost_equal(correct.values, test.values)
+
+if __name__ == '__main__':
+    import nose                                                                      
+    nose.runmodule(argv=[__file__,'-vvs','-x','--pdb', '--pdb-failure'],exit=False)   
