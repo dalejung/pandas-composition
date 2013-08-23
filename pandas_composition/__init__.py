aed983e10506b747ca697c3a75596757711a9241
Dale Jung <dale@dalejung.com>
2013-08-23 03:21:57 -0400

    checked for isclass

--- pandas_composition/__init__.py
+++ pandas_composition/__init__.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import inspect
 
 from pandas_composition.series import UserSeries
 from pandas_composition.frame import UserFrame, _get_meta
@@ -7,7 +8,7 @@ from pandas_composition.metaclass import PandasSuperMeta, PandasMeta
 
 # monkey patch
 def view(self, dtype):
-    if issubclass(dtype, pd.Series):
+    if inspect.isclass(dtype) and issubclass(dtype, pd.Series):
         return dtype(self)
 
     return self._constructor(self.values.view(dtype), index=self.index, name=self.name)
