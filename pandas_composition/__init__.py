35e5d21bbbb1e502d0f76515325301a8a7a8c96a
Dale Jung <dale@dalejung.com>
2013-08-23 01:55:52 -0400

    monkey patching pd.Series.view and numpy.where

--- pandas_composition/__init__.py
+++ pandas_composition/__init__.py
@@ -1,3 +1,30 @@
+import pandas as pd
+import numpy as np
+
 from pandas_composition.series import UserSeries
-from pandas_composition.frame import UserFrame
+from pandas_composition.frame import UserFrame, _get_meta
 from pandas_composition.metaclass import PandasSuperMeta, PandasMeta
+
+# monkey patch
+def view(self, dtype):
+
+    if issubclass(dtype, pd.Series):
+        return dtype(self)
+
+    return self._constructor(self.values.view(dtype), index=self.index, name=self.name)
+
+pd.Series.view = view
+
+def where(condition, *args):
+    if len(args) > 0:
+        res = np.core.multiarray.where(condition, *args)
+    else: 
+        res = np.core.multiarray.where(condition)
+
+    if isinstance(condition, UserSeries):
+        series = pd.Series(res, index=condition.index, name=condition.name)
+        wrapped = condition.copy()
+        wrapped.pobj = series
+        return wrapped
+    return res
+np.where = where
