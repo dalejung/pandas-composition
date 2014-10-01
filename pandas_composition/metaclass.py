e9acb86ad56a86429e215bd26d980f772c2c50de
Dale Jung <dale@dalejung.com>
2014-10-01 13:55:45 -0400

    Change metaclass subclass to ABCMeta

--- pandas_composition/metaclass.py
+++ pandas_composition/metaclass.py
@@ -1,9 +1,10 @@
 import collections
 import inspect
+from abc import ABCMeta
 
 from pandas_composition.base import UserPandasObject, _wrap_method
 
-class PandasMeta(type):
+class PandasMeta(ABCMeta):
     def __new__(cls, name, bases, dct):
         new_attrs = dct
         # test for UserSeries/UserFrame by looking for _pandas_type
